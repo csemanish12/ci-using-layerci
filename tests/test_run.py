417014623f03b2ebec21fcc8699b78cf40bd0e97
Neha Pandey <sahmanish20@gmail.com>
2021-12-09 20:23:57 +0545

    create a minimal flask app

---
+++ tests/test_run.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from app.app import home
+
+
+class TestHome(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_home(self):
+        expected_result = {"msg": "hello world"}
+
+        result = home()
+        self.assertEqual(result, expected_result)
+
+    def test_home2(self):
+        expected_result = {"msgs": "hello world"}
+
+        result = home()
+        self.assertEqual(result, expected_result)
+
+    def tearDown(self) -> None:
+        pass
